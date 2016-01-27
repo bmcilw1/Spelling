bcaed464cb9fb1348fa77fdf86438199934a8062
Brian McIlwain <bmcilw1@gmail.com>
2016-01-27 11:35:47 -0600

    Correct final version

--- spelling.py
+++ spelling.py
@@ -14,43 +14,43 @@ def main():
     cases = int(0)
     case = int(1)
 
-    letters['a'] = 2
-    letters['b'] = 2
-    letters['c'] = 2
-    letters['d'] = 3
-    letters['e'] = 3
-    letters['f'] = 3
-    letters['g'] = 4
-    letters['h'] = 4
-    letters['i'] = 4
-    letters['j'] = 5
-    letters['k'] = 5
-    letters['l'] = 5
-    letters['m'] = 6
-    letters['n'] = 6
-    letters['o'] = 6
-    letters['p'] = 7
-    letters['q'] = 7
-    letters['r'] = 7
-    letters['s'] = 7
-    letters['t'] = 8
-    letters['u'] = 8
-    letters['v'] = 8
-    letters['w'] = 9
-    letters['x'] = 9
-    letters['y'] = 9
-    letters['z'] = 9
-    letters[' '] = 0
+    letters['a'] = '2'
+    letters['b'] = '22'
+    letters['c'] = '222'
+    letters['d'] = '3'
+    letters['e'] = '33'
+    letters['f'] = '333'
+    letters['g'] = '4'
+    letters['h'] = '44'
+    letters['i'] = '444'
+    letters['j'] = '5'
+    letters['k'] = '55'
+    letters['l'] = '555'
+    letters['m'] = '6'
+    letters['n'] = '66'
+    letters['o'] = '666'
+    letters['p'] = '7'
+    letters['q'] = '77'
+    letters['r'] = '777'
+    letters['s'] = '7777'
+    letters['t'] = '8'
+    letters['u'] = '88'
+    letters['v'] = '888'
+    letters['w'] = '9'
+    letters['x'] = '99'
+    letters['y'] = '999'
+    letters['z'] = '9999'
+    letters[' '] = '0'
 
     for line in fileinput.input():
         if not fileinput.isfirstline():
             for character in line:
                 if character not in letters:
                     pass
-                elif len(message) > 0 and letters[character] == message[-1:]:
-                    message += " " + str(letters[character])
+                elif len(message) > 0 and letters[character][:1] == message[-1:]:
+                    message += " " + letters[character]
                 else:
-                    message += str(letters[character])
+                    message += letters[character]
                     
             print("Case #%d: %s" % (case, message))
 
